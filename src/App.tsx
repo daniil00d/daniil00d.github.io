e2d5b935ccb7b9bcc0e3fe3d7a11798f847d9f49
daniil00d <daniil.shenygin.work@gmail.com>
2024-01-03 14:08:02 +0300

    add init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useMemo, useRef, useState } from "react";
+import { GraphCanvas, GraphCanvasRef, useSelection } from "reagraph";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Node {
+  id: number;
+  name: string;
+  father?: string;
+  mother?: string;
+  level: number;
+  nIndex: number;
 }
 
-export default App;
+interface Tree {
+  tree: Node[];
+}
+
+export default function App() {
+  const graphRef = useRef<GraphCanvasRef | null>(null);
+  const [tree, setTree] = useState<null | Node[]>(null);
+
+  useEffect(() => {
+    fetch("/tree.json")
+      .then((res) => res.json())
+      .then((response: Tree) => setTree(response.tree));
+  }, []);
+
+  const nodes = useMemo(() => {
+    const map =
+      tree?.reduce(
+        (prev, curr) => ({ ...prev, [curr.level]: 0 }),
+        {} as Record<number, number>
+      ) || {};
+
+    return (
+      tree?.map((node) => {
+        return {
+          ...node,
+          id: String(node.id),
+          label: node.name,
+          nIndex: map[node.level]++,
+        };
+      }) || []
+    );
+  }, [tree]);
+
+  const edges = useMemo(() => {
+    return (
+      tree?.reduce((prev, curr) => {
+        const acc = [];
+        if (curr.father) {
+          acc.push({
+            id: `${curr.father}->${curr.id}`,
+            source: String(curr.father),
+            target: String(curr.id),
+            label: "",
+          });
+        }
+
+        if (curr.mother) {
+          acc.push({
+            id: `${curr.mother}->${curr.id}`,
+            source: String(curr.mother),
+            target: String(curr.id),
+            label: "",
+          });
+        }
+
+        return [...prev, ...acc];
+      }, [] as { id: string; source: string; target: string; label: string }[]) ||
+      []
+    );
+  }, [tree]);
+
+  const maxLevel = Math.max(...nodes.map((node) => node.level));
+
+  const { selections, actives, onNodeClick, onCanvasClick } = useSelection({
+    ref: graphRef,
+    nodes: nodes,
+    edges: edges,
+    pathSelectionType: "in",
+  });
+
+  return (
+    <GraphCanvas
+      // draggable
+      ref={graphRef}
+      cameraMode="pan"
+      labelFontUrl="/fonts/Roboto/Roboto-Light.ttf"
+      nodes={nodes}
+      edges={edges}
+      selections={selections}
+      actives={actives}
+      onNodeClick={onNodeClick}
+      onCanvasClick={onCanvasClick}
+      layoutType="treeTd2d"
+      layoutOverrides={
+        {
+          getNodePosition: (id: string, { nodes }: { nodes: any }) => {
+            const idx = nodes.findIndex((n: any) => n.id === id);
+            const node = nodes[idx] as Node;
+
+            return {
+              x: 100 * node.nIndex,
+              y: (maxLevel - node.level) * 100,
+              z: 1,
+            };
+          },
+        } as any
+      }
+      labelType="nodes"
+    />
+  );
+}
